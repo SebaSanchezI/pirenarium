1a49c6cb29629b7dd22a9707756abd3842c865d7
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-09-02 13:48:30 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from 'path'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": `${path.resolve(__dirname, "./src/assets/")}`,
+      "@components": `${path.resolve(__dirname, "./src/components/")}`,
+      "@config": `${path.resolve(__dirname, "./src/config/")}`,
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@models": `${path.resolve(__dirname, "./src/models")}`,
+      "@utils": `${path.resolve(__dirname, "./src/utils")}`,
+      "@public": `${path.resolve(__dirname, "./public/")}`,
+    }
+  }
+})
