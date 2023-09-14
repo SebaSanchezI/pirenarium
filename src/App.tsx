c211881899ee2c29b94856cbc106fc76277efd05
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-09-14 12:45:03 +0200

    Create Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container, CssBaseline, ThemeProvider } from '@mui/material'
 import { useThemeContext } from '@context/theme/themeContextProvider'
 import { Navbar } from '@components/Navbar'
+import Landing from './pages/landing/Landing'
 
 export function App () {
   const { theme } = useThemeContext()
@@ -12,7 +13,7 @@ export function App () {
       <Container
         sx={{ height: '100vh', paddingTop: '98px', paddingBottom: '24px' }}
       >
-        
+        <Landing />
       </Container>
     </ThemeProvider>
   )
