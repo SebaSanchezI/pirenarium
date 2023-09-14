d9df86fcaa6c9a5fb985879a6aaf719cca38f1b1
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-09-14 11:30:26 +0200

    Create Page container

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Container, CssBaseline, ThemeProvider } from '@mui/material'
 import { useThemeContext } from '@context/theme/themeContextProvider'
+import { Navbar } from '@components/Navbar'
 
 export function App () {
   const { theme } = useThemeContext()
@@ -7,6 +8,12 @@ export function App () {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Navbar />
+      <Container
+        sx={{ height: '100vh', paddingTop: '98px', paddingBottom: '24px' }}
+      >
+        
+      </Container>
     </ThemeProvider>
   )
 }
