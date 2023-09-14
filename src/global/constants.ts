123ee245d4bc7894c2f83dedc28a1c8ee7deacb2
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-09-14 12:41:48 +0200

    Add section ids

--- src/global/constants.ts
+++ src/global/constants.ts
@@ -1,7 +1,15 @@
-export const TITLES = {
+export const TITLE = {
   MAIN: 'Pirenarium Hotel',
   SERVICES: 'Servicios',
   ROOMS: 'Habitaciones',
   FAQ: 'Preguntas Frecuentes',
   CONTACT: 'Contacto'
+}
+
+export const SECTION_IDS = {
+  HOME: 'home',
+  SERVICES: 'services',
+  ROOMS: 'rooms',
+  FAQ: 'faq',
+  CONTACT: 'contact'
 }
