7fc308b77726d86c5a5e6ae0a802a8de29ed0a8f
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-09-28 13:27:48 +0200

    Add fullscreen article

--- src/components/article/SectionComponent.tsx
+++ src/components/article/SectionComponent.tsx
@@ -4,12 +4,20 @@ import { Box, Typography } from '@mui/material'
 interface SectionComponentProps {
   sectionId: string
   title: string
+  fullscreen?: boolean
   children: ReactNode
 }
 
-const SectionComponent: React.FC<SectionComponentProps> = ({ sectionId, title, children }) => {
+const SectionComponent: React.FC<SectionComponentProps> = ({ sectionId, title, fullscreen, children }) => {
   return (
-    <Box id={sectionId} component='section' display='flex' flexDirection='column' marginBottom='32px'>
+    <Box
+      id={sectionId}
+      component='section'
+      display='flex'
+      flexDirection='column'
+      marginBottom='32px'
+      minHeight={(fullscreen !== null && fullscreen !== undefined) ? '100vh' : 0}
+    >
       <Typography variant='h3' textAlign='start' marginBottom='16px'>
         {title}
       </Typography>
