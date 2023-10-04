5ab91359ffd27da20f34d5fe17ea07f2b758ad49
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-10-04 13:30:58 +0200

    Change link names

--- src/components/contact/data/contactData.ts
+++ src/components/contact/data/contactData.ts
@@ -7,18 +7,18 @@ export const CONTACT_TITLES = {
 }
 
 export const CONTACT_DATA = {
-  ADDRESS: 'Sabiñanigo, Huesca, España',
-  PHONE: '34567890987',
-  EMAIL: 'info@pirenarium.com'
+  ADDRESS: 'Av. del Ejército 27, 22609 Sabiñánigo, Huesca, España',
+  PHONE: '34657479796',
+  EMAIL: 'alberguejuvenilpirenarium@gmail.com'
 }
 
 export const BOOK_LINKS = {
   BOOKING: {
-    name: 'Booking',
+    name: 'Booking.com',
     url: 'https://www.booking.com/hotel/es/albergue-pirenarium-sabinanigo.es.html?aid=304142&label=Share-PljAdo%401693486549'
   },
   DIRECT_BOOK: {
-    name: 'Direct Book',
+    name: 'DirectBook.com',
     url: 'https://direct-book.com/properties/alberguepirenarium-direct?locale=en&items[0][adults]=2&items[0][children]=0&items[0][infants]=0&currency=EUR&checkInDate=2023-10-03&checkOutDate=2023-10-04&trackPage=yes'
   }
 }
