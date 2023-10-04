9dcfadd01a6761d5b1e9641748aefae1113c507e
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-10-04 12:57:03 +0200

    Add new images in room section

--- src/components/imageList/data/imageListData.ts
+++ src/components/imageList/data/imageListData.ts
@@ -5,6 +5,14 @@ import room2 from '@assets/rooms/room2.jpeg'
 import room3 from '@assets/rooms/room3.jpeg'
 import room4 from '@assets/rooms/room4.jpeg'
 import room5 from '@assets/rooms/room5.jpeg'
+import room6 from '@assets/rooms/room6.jpeg'
+import room7 from '@assets/rooms/room7.jpeg'
+import room8 from '@assets/rooms/room8.jpeg'
+import commonSpace1 from '@assets/commonSpaces/commonSpaces1.jpeg'
+import commonSpace2 from '@assets/commonSpaces/commonSpaces2.jpeg'
+import commonSpace3 from '@assets/commonSpaces/commonSpaces3.jpeg'
+import commonSpace4 from '@assets/commonSpaces/commonSpaces4.jpeg'
+import commonSpace5 from '@assets/commonSpaces/commonSpaces5.jpeg'
 
 export const IMAGE_ROOM_LIST: ImageItemInterface[] = [
   {
@@ -16,6 +24,14 @@ export const IMAGE_ROOM_LIST: ImageItemInterface[] = [
     title: 'room2'
   },
   {
+    img: commonSpace5,
+    title: 'commonSpace5'
+  },
+  {
+    img: room7,
+    title: 'room7'
+  },
+  {
     img: room3,
     title: 'room3'
   },
@@ -24,7 +40,31 @@ export const IMAGE_ROOM_LIST: ImageItemInterface[] = [
     title: 'room4'
   },
   {
+    img: commonSpace3,
+    title: 'commonSpace3'
+  },
+  {
     img: room5,
     title: 'room5'
+  },
+  {
+    img: commonSpace4,
+    title: 'commonSpace4'
+  },
+  {
+    img: room6,
+    title: 'room6'
+  },
+  {
+    img: room8,
+    title: 'room8'
+  },
+  {
+    img: commonSpace1,
+    title: 'commonSpace1'
+  },
+  {
+    img: commonSpace2,
+    title: 'commonSpace2'
   }
 ]
