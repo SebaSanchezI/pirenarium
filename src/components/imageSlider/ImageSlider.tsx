c0358b8602bab3f7b928d6fbaf6270cee3118683
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-10-04 12:27:08 +0200

    Add handle nullish case in image slider

--- src/components/imageSlider/ImageSlider.tsx
+++ src/components/imageSlider/ImageSlider.tsx
@@ -5,31 +5,30 @@ import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord'
 
-import { Slice } from "./models/imageSlider.type"
-import { Container, ImageSliderContainer, Img, ArrrowButtonLeft, ArrrowButtonRight } from "./styles/style"
+import { type Slice } from './models/imageSlider.type'
+import { Container, ImageSliderContainer, Img, ArrrowButtonLeft, ArrrowButtonRight } from './styles/style'
 
 interface ImageSliderProps {
-  slices: Slice[],
-  showArrows?: boolean,
-  autoPlay?: boolean,
-  width?: string,
+  slices: Slice[]
+  showArrows?: boolean
+  autoPlay?: boolean
+  width?: string
   height?: string
 }
 
-const ImageSlider = ( { slices, showArrows, autoPlay, width, height }: ImageSliderProps) => {
- 
+const ImageSlider = ({ slices, showArrows, autoPlay, width, height }: ImageSliderProps) => {
   const [sliceSelected, setSliceSelected] = useState<number>(0)
 
   const handlePreviousSlice = () => {
     setSliceSelected((prevValue: number) => {
-      if(prevValue === 0) return slices.length - 1
+      if (prevValue === 0) return slices.length - 1
       return prevValue - 1
     })
   }
 
   const handleNextSlice = () => {
     setSliceSelected((prevValue: number) => {
-      if(prevValue === slices.length - 1) return 0
+      if (prevValue === slices.length - 1) return 0
       return prevValue + 1
     })
   }
@@ -37,14 +36,14 @@ const ImageSlider = ( { slices, showArrows, autoPlay, width, height }: ImageSlid
   if (slices.length < 1) return
 
   useEffect(() => {
-    if (autoPlay) {
+    if (autoPlay === true) {
       const interval = setInterval(() => {
         handleNextSlice()
       }, 2000)
-      return () => clearInterval(interval)
+      return () => { clearInterval(interval) }
     }
   }, [])
-  
+
   return (
     <Container>
       <ImageSliderContainer
@@ -53,17 +52,16 @@ const ImageSlider = ( { slices, showArrows, autoPlay, width, height }: ImageSlid
         height: `${height}`
       }}
       >
-        {showArrows
-          &&
+        {(showArrows === true) &&
           <ArrrowButtonLeft size='large' onClick={handlePreviousSlice}>
             <ArrowBackIosIcon sx={{ width: '2rem', height: '2rem' }} viewBox='0 0 14 24' />
           </ArrrowButtonLeft>
         }
-        
+
         {
-          slices.map(( s: Slice, i )  => (
-            sliceSelected === i
-            && <Img
+          slices.map((s: Slice, i) => (
+            sliceSelected === i &&
+            <Img
               key={i}
               src={s.src}
               alt={s.alt}
@@ -71,21 +69,20 @@ const ImageSlider = ( { slices, showArrows, autoPlay, width, height }: ImageSlid
             />
           ))
         }
-        {showArrows
-        &&
+        {(showArrows === true) &&
           <ArrrowButtonRight size='large' onClick={handleNextSlice}>
             <ArrowForwardIosIcon sx={{ width: '2rem', height: '2rem' }}/>
           </ArrrowButtonRight>
         }
-      
+
         <Box position='absolute' display='flex' bottom='12px'sx={{ borderRadius: '12px' }}>
-          {slices.map((_,idx) => (
-            <IconButton key={idx} size='small' onClick={() => setSliceSelected(idx)} sx={{ padding: 0, border: 'none' }}>
+          {slices.map((_, idx) => (
+            <IconButton key={idx} size='small' onClick={() => { setSliceSelected(idx) }} sx={{ padding: 0, border: 'none' }}>
               <FiberManualRecordIcon
-              sx={{ 
-                width: '1rem', 
+              sx={{
+                width: '1rem',
                 height: '1rem',
-                color: `${idx === sliceSelected ? 'white' : '#363535' }`,
+                color: `${idx === sliceSelected ? 'white' : '#363535'}`,
                 transition: '0.5s ease-in'
               }}
               />
