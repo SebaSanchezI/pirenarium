bc831d8037a20f1ea1f2dbecdf202319e8d42782
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-10-05 13:44:17 +0200

    fix error in main

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import { ThemeContextProvider } from '@context/theme/themeContextProvider.tsx'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeContextProvider>
       <App />
