2589453f3e471b8f19da13ff60a14e59a6061411
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-10-05 12:13:06 +0200

    Add new faq

--- src/components/accordionList/data/accordionDataList.ts
+++ src/components/accordionList/data/accordionDataList.ts
@@ -8,7 +8,38 @@ export const ACCORDION_DATA_LIST: AccordionItemInterface[] = [
   },
   {
     panelId: 2,
-    title: '¿Dónde nos encontramos?',
-    description: 'Pirenarium Hotel se encuentra en la ciudad de Sabiñanigo en la provincia de Huesca.'
+    title: 'Check-In y Check-Out',
+    description: 'El horario de Ingreso al complejo es de las 16:00 a las 22:00. El horario de Salida es de las 06:00 a las 11:00.'
+  },
+  {
+    panelId: 3,
+    title: 'Cancelación/Prepago',
+    description: 'Las condiciones de cancelación y de pago por adelantado pueden variar según el tipo de alojamiento. Consulta las condiciones de la habitación seleccionada por cualquiera de nuestros medios de comunicación.'
+  },
+  {
+    panelId: 4,
+    title: '¿Cúanto cuesta hospedarse?',
+    description: 'Los precios en Albergue Pirenarium pueden variar en función de la estancia (fechas, condiciones del hotel, etc.). Elige tus fechas para ver el precio.'
+  },
+  {
+    panelId: 5,
+    title: '¿Cuáles son los tipos de habitaciones?',
+    description: 'El albergue cuenta con 2 tipos: habitaciones dobles y habitaciones compartida mixta de 6 camas. En este alojamiento no hay cunas ni camas supletorias.'
+  },
+  {
+    panelId: 6,
+    title: 'Medios de pagos',
+    description: 'Actualmente aceptamos solo pagos con tarjeta (no aceptamos pagos en efectivo). Se aceptan las tarjetas VISA, MASTERCARD, AMERICAN EXPRESS y MAESTRO y se reserva el derecho de cargar de forma temporal una cantidad antes de la entrada.'
+  },
+  {
+    panelId: 7,
+    title: 'Mascotas',
+    description: 'Actualmente no aceptamos mascotas.'
+  },
+  {
+    panelId: 8,
+    title: '¿Qué se puede hacer?',
+    description: 'Se puede almorzar y cenar en nuestro restorante. Se pueden realizar eventos privados: fiestas, casamientos, etc. Se puede disfrutar de espectáculos al aire libre y música en vivo.'
   }
+
 ]
