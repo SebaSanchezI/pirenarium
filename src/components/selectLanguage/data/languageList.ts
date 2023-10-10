6ed93bb788c43a8c895b14550442f794c527c948
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-10-10 14:29:15 +0200

    Create select language

---
+++ src/components/selectLanguage/data/languageList.ts
@@ -0,0 +1,6 @@
+import { type Language } from '../model/language.type'
+
+export const LANGUAGE_LIST: Language[] = [
+  { id: 1, value: 'es-ES', label: 'Español' },
+  { id: 2, value: 'en-GB', label: 'English' }
+]
