ea62d105b725fd6d50f48fa3f7efa48af47b4b14
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-10-11 13:09:41 +0200

    Add translation in faq section

--- src/components/accordionList/data/accordionDataList.ts
+++ src/components/accordionList/data/accordionDataList.ts
@@ -1,45 +1,12 @@
 import { type AccordionItemInterface } from '../components/model/accordionItem.type'
 
 export const ACCORDION_DATA_LIST: AccordionItemInterface[] = [
-  {
-    panelId: 1,
-    title: '¿Cómo reservar?',
-    description: 'Para poder reservar una habitacion tenes que ingresar en Booking o en Direct Book. Los links los podras encontrar en la sección de Contactos.'
-  },
-  {
-    panelId: 2,
-    title: 'Check-In y Check-Out',
-    description: 'El horario de Ingreso al complejo es de las 16:00 a las 22:00. El horario de Salida es de las 06:00 a las 11:00.'
-  },
-  {
-    panelId: 3,
-    title: 'Cancelación/Prepago',
-    description: 'Las condiciones de cancelación y de pago por adelantado pueden variar según el tipo de alojamiento. Consulta las condiciones de la habitación seleccionada por cualquiera de nuestros medios de comunicación.'
-  },
-  {
-    panelId: 4,
-    title: '¿Cúanto cuesta hospedarse?',
-    description: 'Los precios en Albergue Pirenarium pueden variar en función de la estancia (fechas, condiciones del hotel, etc.). Elige tus fechas para ver el precio.'
-  },
-  {
-    panelId: 5,
-    title: '¿Cuáles son los tipos de habitaciones?',
-    description: 'El albergue cuenta con 2 tipos: habitaciones dobles y habitaciones compartida mixta de 6 camas. En este alojamiento no hay cunas ni camas supletorias.'
-  },
-  {
-    panelId: 6,
-    title: 'Medios de pagos',
-    description: 'Actualmente aceptamos solo pagos con tarjeta (no aceptamos pagos en efectivo). Se aceptan las tarjetas VISA, MASTERCARD, AMERICAN EXPRESS y MAESTRO y se reserva el derecho de cargar de forma temporal una cantidad antes de la entrada.'
-  },
-  {
-    panelId: 7,
-    title: 'Mascotas',
-    description: 'Actualmente no aceptamos mascotas.'
-  },
-  {
-    panelId: 8,
-    title: '¿Qué se puede hacer?',
-    description: 'Se puede almorzar y cenar en nuestro restorante. Se pueden realizar eventos privados: fiestas, casamientos, etc. Se puede disfrutar de espectáculos al aire libre y música en vivo.'
-  }
-
+  { panelId: 1 },
+  { panelId: 2 },
+  { panelId: 3 },
+  { panelId: 4 },
+  { panelId: 5 },
+  { panelId: 6 },
+  { panelId: 7 },
+  { panelId: 8 }
 ]
