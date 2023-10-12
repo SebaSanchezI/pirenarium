a3de9153077283ddc026bb923964c97f691f59a2
SebaSanchezI <72668007+SebaSanchezI@users.noreply.github.com>
2023-10-12 13:52:01 +0200

    add flag images

--- src/components/selectLanguage/SelectLanguage.tsx
+++ src/components/selectLanguage/SelectLanguage.tsx
@@ -5,6 +5,7 @@ import { Box, FormControl, ListItemText, MenuItem } from '@mui/material'
 import Select, { type SelectChangeEvent } from '@mui/material/Select'
 
 import { type Language } from './model/language.type'
+import { FlagImage } from './styles/styles'
 
 interface SelectLanProps {
   languages: Language[]
@@ -12,7 +13,11 @@ interface SelectLanProps {
 
 const SelectLanguage: React.FC<SelectLanProps> = ({ languages }) => {
   const [language, setLanguage] = useState<string>('')
+  const [selectedLan, setSelectedLan] = useState<Language | undefined>()
+
   const handleChangeLan = async (event: SelectChangeEvent): Promise<void> => {
+    const wanted = languages.find(l => l.value === event.target.value) ?? languages[0]
+    setSelectedLan(wanted)
     setLanguage(event.target.value)
     await i18next.changeLanguage(event.target.value)
   }
@@ -20,6 +25,8 @@ const SelectLanguage: React.FC<SelectLanProps> = ({ languages }) => {
   useEffect(() => {
     const lang = localStorage.getItem('i18nextLng')
     setLanguage(lang ?? '')
+    const wanted = languages.find(l => l.value === lang) ?? languages[0]
+    setSelectedLan(wanted)
   }, [])
 
   return (
@@ -32,14 +39,17 @@ const SelectLanguage: React.FC<SelectLanProps> = ({ languages }) => {
           label='Language'
           onChange={handleChangeLan}
           type='outlined'
+          autoWidth
+          renderValue={() => <FlagImage src={selectedLan?.flagImg} alt={`flag-${selectedLan?.value}`} />}
         >
           {
             languages.map(({ id, value, label, flagImg }: Language) => (
               <MenuItem
                 key={id}
                 value={value}
+                sx={{ display: 'flex', flexDirection: 'row', width: '100%' }}
               >
-                <img src={flagImg} alt={`flag-${label}`} />
+                <FlagImage src={flagImg} alt={`flag-${label}`} />
                 <ListItemText sx={{ marginLeft: '16px' }}>{label}</ListItemText>
               </MenuItem>
             ))
